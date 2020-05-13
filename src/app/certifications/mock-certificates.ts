62fd78e04122d47d15fc33450e652a21d7cd4c15
Jordan Bordonado <jbordo_casu@outlook.fr>
2020-05-13 15:29:43 +0200

    enrich certifications section

--- src/app/certifications/mock-certificates.ts
+++ src/app/certifications/mock-certificates.ts
@@ -2,6 +2,46 @@ import { Certificate } from './certificate.model';
 
 export const CERTIFICATES: Certificate[] = [
   {
+    name: 'Big Data analysis with Scala and Spark',
+    authorityLogo: 'coursera-logo.png',
+    authorityName: 'Coursera',
+    issuingDate: '2018 / 01',
+    link:
+      'https://www.coursera.org/account/accomplishments/verify/ZTKDG2DUPSPB',
+  },
+  {
+    name: 'Parallel Programming',
+    authorityLogo: 'coursera-logo.png',
+    authorityName: 'Coursera',
+    issuingDate: '2017 / 12',
+    link:
+      'https://www.coursera.org/account/accomplishments/verify/CBNF9HJGDC3G',
+  },
+  {
+    name: 'Functionnal Program Design in Scala',
+    authorityLogo: 'coursera-logo.png',
+    authorityName: 'Coursera',
+    issuingDate: '2017 / 08',
+    link:
+      'https://www.coursera.org/account/accomplishments/verify/HJKYLLPASAMP',
+  },
+  {
+    name: 'Functionnal Programming Principles in Scala',
+    authorityLogo: 'coursera-logo.png',
+    authorityName: 'Coursera',
+    issuingDate: '2017 / 08',
+    link:
+      'https://www.coursera.org/account/accomplishments/verify/GLEC4UNB6F6L',
+  },
+  {
+    name: 'Hadoop Platform and Application Framework',
+    authorityLogo: 'coursera-logo.png',
+    authorityName: 'Coursera',
+    issuingDate: '2017 / 06',
+    link:
+      'https://www.coursera.org/account/accomplishments/verify/QDS8V6HH3AQ3',
+  },
+  {
     name: 'Oracle Certified Associate, Java SE 8 Programmer',
     authorityLogo: 'oracle-logo.jpg',
     authorityName: 'Oracle',
